e44f23cff7b0c9925217397b70cd74c80dd35ba0
sasankaweera123 <71062887+sasankaweera123@users.noreply.github.com>
2022-05-19 11:16:13 +0530

    Create LED_Dimmer.ino

---
+++ Session_02/LED_Dimmer/LED_Dimmer.ino
@@ -0,0 +1,21 @@
+#define LED  5 // The pin the LED is connected to 5 
+// for analogwrite we need to use 1-6 pin to get expexted outcomes
+
+// Can use int LED = 5 also
+
+void setup() {
+  pinMode(LED, OUTPUT); // Declare the LED as an output
+}
+
+void loop() {
+    // brightness ratio 0-255
+  analogWrite(LED, 0); // Turn the LED brightness 0
+  delay(500); // Give a Delay 
+  analogWrite(LED, 10); // Turn the LED brightness 10
+  delay(500); // Give a Delay 
+  analogWrite(LED, 100); // Turn the LED brightness 100
+  delay(500); // Give a Delay 
+  analogWrite(LED, 150); // Turn the LED brightness 150
+  delay(500); // Give a Delay 
+  analogWrite(LED, 255); // Turn the LED brightness 255
+}
